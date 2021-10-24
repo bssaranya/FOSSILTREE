3874aa05f1ae20c6b335a0be96be2cee5f6b1a36
jayapal <jayapal.d@gmail.com>
2021-10-24 14:44:09 +0530

    staticadded and serialized data

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .products import products
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 
 @api_view(['GET'])
 def getRoutes(request):
-    routes = [
-
-
-    ]
+    routes = []
     return Response(routes)
 
 @api_view(['GET'])
 def getProducts(requests):
-    return Response(products)
+
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(requests, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product,many=False)
+    return Response(serializer.data)
+
